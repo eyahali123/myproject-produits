a98fea26b5fa5a2ebbd8984fa1fc46d2d0659a52
eyahali123 <eyahali034@gmail.com>
2022-10-23 22:04:38 +0100

    add-file

--- produits/src/test/java/com/example/demo/ProduitsApplicationTests.java
+++ produits/src/test/java/com/example/demo/ProduitsApplicationTests.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.entities.Categorie;
 import com.example.demo.entities.Produit;
 import com.example.demo.repos.ProduitRepository;
 
@@ -14,7 +16,7 @@ class ProduitsApplicationTests {
 private ProduitRepository produitRepository;
 @Test
 public void testCreateProduit() {
-Produit prod = new Produit("PC Dell",2200.500,new Date());
+Produit prod = new Produit("PC Acer",2500.500,new Date());
 produitRepository.save(prod);
 }
 @Test
@@ -31,16 +33,95 @@ Produit p = produitRepository.findById(1L).get();
 p.setPrixProduit(1000.0);
 produitRepository.save(p);
 }
+
 @Test
 public void testDeleteProduit()
 {
-produitRepository.deleteById(1L);;
+produitRepository.deleteById(1L);
 }
 
 @Test
 public void testListerTousProduits()
 {
 List<Produit> prods = produitRepository.findAll();
+for (Produit p : prods)
+{
+System.out.println(p);
+}
+
+}
+
+@Test
+public void testFindByNomProduit()
+{
+List<Produit> prods = produitRepository.findByNomProduit("iphone X");
+
+for (Produit p : prods)
+{
+System.out.println(p);
+}
+
+}
+
+@Test
+public void testFindByNomProduitContains ()
+{
+List<Produit> prods=produitRepository.findByNomProduitContains("P");
+
+for (Produit p : prods)
+{
+System.out.println(p);
+} }
+@Test
+public void testfindByNomPrix()
+{
+List<Produit> prods = produitRepository.findByNomPrix("iphone X", 1000.0);
+for (Produit p : prods)
+{
+System.out.println(p);
+}
+
+}
+@Test
+public void testfindByCategorie()
+{
+Categorie cat = new Categorie();
+cat.setIdCat(1L);
+List<Produit> prods = produitRepository.findByCategorie(cat);
+for (Produit p : prods)
+{
+System.out.println(p);
+}
+
+}
+
+
+@Test
+public void findByCategorieIdCat()
+{
+List<Produit> prods = produitRepository.findByCategorieIdCat(1L);
+for (Produit p : prods)
+{
+System.out.println(p);
+}
+
+}
+@Test
+public void testfindByOrderByNomProduitAsc()
+{
+List<Produit> prods =
+
+produitRepository.findByOrderByNomProduitAsc();
+for (Produit p : prods)
+{
+System.out.println(p);
+}
+
+}
+@Test
+public void testTrierProduitsNomsPrix()
+{
+List<Produit> prods = produitRepository.trierProduitsNomsPrix();
 for (Produit p : prods)
 {
 System.out.println(p);
